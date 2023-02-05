7833f1e8ad925b7eef48b67e593a0037dbeb11ef
Lars Michael <lars.michael.dk@gmail.com>
2023-02-05 20:31:41 +0100

    Frame introduced

---
+++ BowlingGame/Frame.cs
@@ -0,0 +1,9 @@
+﻿namespace BowlingGame
+{
+    public class Frame
+    {
+        public int Number { get; }
+
+        public Frame(int number) => Number = number;
+    }
+}
--- BowlingGame/Game.cs
+++ BowlingGame/Game.cs
@@ -2,6 +2,8 @@
 {
     public class Game
     {
+        private int[] rolls = new int[21];
+
         public Game(string playerName)
         {
             if (string.IsNullOrEmpty(playerName)) 
@@ -9,9 +11,17 @@
                 throw new ArgumentNullException(nameof(playerName));
             }
             PlayerName = playerName;
+
+            Frames = new Frame[10];
         }
 
         public string PlayerName { get; }
+
+        public int CurrentFrame { get; } = 1;
+
+        public Frame[] Frames { get; }
+
+        public int Score { get; }
 
         public void Roll(int pins)
         {
